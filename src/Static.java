7ad8ba8cbd8d54407d3b7b6315e831e477d5f3df
ellerkadi <kadieller@gmail.com>
2024-11-05 14:41:08 +0200

    Static

---
+++ src/Static.java
@@ -0,0 +1,17 @@
+public class Static {
+    int number;
+    static String name = "Kadi";
+    private static int totalAccounts = 1;
+
+    public static void main(String[] args) {
+        System.out.println(name);
+    }
+
+    public double multiply() {
+        double a = 2;
+        double b = 4;
+        return a * b;
+    }
+
+
+}
