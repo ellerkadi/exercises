8a1f727a653edaf230400957d47bd4bcac28ce2c
ellerkadi <kadieller@gmail.com>
2024-11-06 15:40:03 +0200

    Escape Sequences

---
+++ src/EscapeSequences.java
@@ -0,0 +1,15 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+
+        // ESCAPE SEQUENCES
+        System.out.println("Hello \bWorld!"); //backspace
+        System.out.println("Hello \tWorld!"); //tab
+        System.out.println("Hello \nWorld!"); //newline
+        System.out.println("Hello \rWorld!"); //carriage return
+        System.out.println("Hello \"World!\""); //double quote
+        System.out.println("Hello \'World!\'"); //single quote
+        System.out.println("Hello \\World!"); //backlash
+
+
+    }
+}
