2797e70f6ced7265d5f0e5e0e525b779693586d3
ellerkadi <kadieller@gmail.com>
2024-11-06 16:18:57 +0200

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,11 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int [] m = new int[10];
+        m[0] = 1;
+        System.out.println(m[0]);
+        System.out.println(java.util.Arrays.toString(m));
+
+        int [] m2 = {2,7,3,6,1};
+        System.out.println(java.util.Arrays.toString(m2));
+    }
+}
